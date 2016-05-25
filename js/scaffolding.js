dfa2a7adbf8bab1df067a752c852f8154093168e
brandon <brandon@behemoth.ca>
2016-05-25 15:25:52 -0700

    Addl layout improvements

--- js/scaffolding.js
+++ js/scaffolding.js
@@ -1,14 +1,13 @@
 $("a[href^='#'].sg-nav-link").on('click', function(e) {
-   // prevent default anchor click behavior
-   e.preventDefault();
-   // animate
-   $('html, body').animate({
-       scrollTop: $(this.hash).offset().top
-     }, 500, function(){
+    // prevent default anchor click behavior
+    e.preventDefault();
+    // animate
+    $('html, body').animate({
+        scrollTop: $(this.hash).offset().top
+    }, slow, function() {
 
-       // when done, add hash to url
-       // (default click behaviour)
-       window.location.hash = this.hash;
-     });
-
+        // when done, add hash to url
+        // (default click behaviour)
+        window.location.hash = this.hash;
+    });
 });
