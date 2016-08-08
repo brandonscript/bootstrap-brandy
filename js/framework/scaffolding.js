466af9aeafd8d11c4c93df60940642225b969d49
brandon <brandon@behemoth.ca>
2016-08-08 15:04:29 -0700

    Resolve #10 (menu items don't correctly update url anchor link)

--- js/framework/scaffolding.js
+++ js/framework/scaffolding.js
@@ -2,14 +2,14 @@
     $("a[href^='#'].sg-nav-link").on('click', function(e) {
         // prevent default anchor click behavior
         e.preventDefault()
+        var hash = this.hash.replace('#','')
         // animate
         $('html, body').animate({
             scrollTop: $(this.hash).offset().top
         }, 'slow', function() {
-
             // when done, add hash to url
             // (default click behaviour)
-            window.location.hash = this.hash
+            window.location.hash = hash
         })
     })
 }(jQuery))
