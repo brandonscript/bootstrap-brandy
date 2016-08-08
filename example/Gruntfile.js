14448c8de2f7389b80e1ca98b95c04f81e937b9a
brandon <brandon@behemoth.ca>
2016-08-08 15:02:26 -0700

    Simplifies example gruntfile to remove var declaration

--- example/Gruntfile.js
+++ example/Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt)
-    var path = require('path')
     grunt.initConfig({
         subgrunt: {
             brandy: {
@@ -8,7 +7,7 @@ module.exports = function(grunt) {
                     npmInstall: true
                 },
                 projects: {
-                    'node_modules/bootstrap-brandy': ['default', '--assets=' + path.resolve()]
+                    'node_modules/bootstrap-brandy': ['default', '--assets=' + require('path').resolve()]
                 }
             }
         }
